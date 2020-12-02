028e69e805f2b12750b3b5b7faff4be1090651ee
valixon <mr.valixon.1991@gmail.com>
2020-12-02 11:50:11 +0500

    init project

---
+++ src/main/java/com/example/secondprojectspring/SecondprojectspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.secondprojectspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondprojectspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondprojectspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/secondprojectspring/SecondprojectspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secondprojectspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondprojectspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
